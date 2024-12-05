a07234e13d458e004c8115f87dc20f5c5bb3e8ef
Jason Luong <jjluong009@gmail.com>
2024-12-04 21:36:29 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+public static void main (String[] args) throws Exception {
+    System.out.println(largestNum());
+}
+
+public static int largestNum() {
+  
+ArrayList<Integer> Numbers = new ArrayList<Integer>();
+int[] myArray = {15, 42, 3, 64, 72, 89, 415};
+
+for (int i = 0; i <= myArray.length - 1; i++) {
+  Numbers.add(myArray[i]);
+}
+
+Collections.sort(Numbers);
+return (Numbers.get(Numbers.size() - 5));
+    }
+
+}
+
+// example of a method that returns the largest integer in an array
